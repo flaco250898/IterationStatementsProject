e1f71bce2a43a70c7aeec9c91e56899e9b66510c
Liam Torres <liam.torreslopez@gmail.com>
2022-10-15 19:20:05 -0700

    I created for, foreach, and do loops

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -12,12 +12,12 @@ namespace IterationStatements
 
                 //Create a List called "numbers" - DONE!
                 var numbers = new List<int>();
-            
+
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
             //initialize the variable with a value of 0
 
-
+            var myNumber = 0; 
 
             // Create a do-while loop and use the template below:
 
@@ -26,17 +26,24 @@ namespace IterationStatements
 
                 // Then add num to the collection - numbers
                     // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+               
 
             } // <---- While your variable is less than 100
 
-
+            do
+            {
+                myNumber++;
+                numbers.Add(myNumber);
+            } while (myNumber < 100);
 
             // Create a while loop
             // <--- While num is less than 200
+             while (myNumber < 200)
             {
                 // Increment num by 1
+                myNumber++;
                 // Then add num to the collection numbers
+                numbers.Add(myNumber);
                     //HINT: copy how this was done in the do while loop
 
             }
@@ -48,7 +55,11 @@ namespace IterationStatements
 
             // Create a foreach loop using the collection - numbers
                 //In the scope of the foreach loop, print each number in numbers
-             
+             foreach (var item in numbers)
+            {
+                Console.WriteLine(item); 
+
+            }
 
 
             Console.WriteLine("");
@@ -62,7 +73,9 @@ namespace IterationStatements
                 // Decrement i by 1
 
             //start for loop here
+            for (int i = 199; i <= numbers.Count && i >=0; i--)
             {
+                Console.WriteLine($"{numbers[i]}");
                 // place numbers[i] inside of the Console.WriteLine() method
             }
 
